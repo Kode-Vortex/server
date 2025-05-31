dca3ffae1fa8b44e75b9aaced5a9b8c8bc6c2640
Harsh Verma <harshverma8433@gmail.com>
2025-05-31 13:21:29 +0530

    payment

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ ConnectToDataBase()
 
 app.use(cors({
     
-    // origin: "https://www.kodevortex.in" ,
-    origin: "http://localhost:5173" ,
+    origin: "https://www.kodevortex.in" ,
+    // origin: "http://localhost:5173" ,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"], 
